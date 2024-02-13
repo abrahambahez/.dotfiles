736f510f77c357bdbf092d8bbb532d84a09ee635
abraham bahez <s.abrahambh@gmail.com>
2024-02-12 18:53:19 -0600

    To rebase

--- scripts/clone-obsidian-base.sh
+++ scripts/clone-obsidian-base.sh
@@ -11,7 +11,7 @@ else
 fi
 
 # Copy base directories
-cp -r $sourceDir.obsidian/* $targetDir
+cp -r $sourceDir.obsidian/ $targetDir
 cp -r $sourceDir.pandoc/ $targetDir
 cp "$sourceDir"gitignore $targetDir.gitignore
 
