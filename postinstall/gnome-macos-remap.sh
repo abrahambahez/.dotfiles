a0db468560b187d4f39a2856b7d3db6ea3ac6e80
Sergio Abraham Barrera <tacto@sabhz.com>
2025-03-31 10:55:35 -0600

    Feat: Add MacOs remap support

---
+++ postinstall/gnome-macos-remap.sh
@@ -0,0 +1,6 @@
+echo "Instalar extensión Gnome MacOs remap"
+cd ~/Descargas/
+git clone https://github.com/petrstepanov/gnome-macos-remap-wayland
+cd gnome-macos-remap-wayland
+chmod +x ./install.sh
+./install.sh
