cbf3aa2101c7a31a4791065ca2495ea5c1487f16
abraham bahez <s.abrahambh@gmail.com>
2025-04-01 10:39:43 -0600

    Feat: Add stow espanso to install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Check dependencies git stow, antibody
 
-for i in stow antibody nvim zsh kitty 
+for i in stow antibody nvim zsh ghostty espanso
 do 
     if ! command -v $i &> /dev/null
     then
@@ -17,7 +17,10 @@ done
 
 stow nvim
 stow zsh
-stow kitty
+stow ghostty
+
+# Try to stow to espanso location in macos
+stow -t ~/Library/Application\ Support -d ~/.dotfiles espanso
 
 # Use antibody
 # Consider change antibody for antidote (newer implementation) https://github.com/mattmc3/antidote
