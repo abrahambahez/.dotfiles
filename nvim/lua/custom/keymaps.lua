24967fb303c0cf22dc2879255ffe60498c0ab163
Abraham Bahez <s.abrahambh@gmail.com>
2025-06-09 21:06:51 -0600

    Feat: Keymaps to copy and delete files

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -6,6 +6,30 @@ local function map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
+-- COPY CURRENT FILE TO CLIPBOARD
+vim.keymap.set('n', '<leader>cf', function()
+  local filename = vim.fn.expand '%:t'
+  if filename ~= '' then
+    vim.fn.setreg('+', filename)
+    print('Copied to clipboard: ' .. filename)
+  else
+    print 'No file to copy'
+  end
+end, { desc = 'Copy filename to clipboard' })
+
+-- DELETE CURRENT FILE
+vim.keymap.set('n', '<leader>df', function()
+  local file = vim.fn.expand '%:p'
+  if file ~= '' then
+    local choice = vim.fn.confirm('Delete "' .. vim.fn.fnamemodify(file, ':t') .. '"?', '&Yes\n&No', 2)
+    if choice == 1 then
+      vim.cmd 'bdelete!'
+      os.remove(file)
+      print('Deleted: ' .. vim.fn.fnamemodify(file, ':t'))
+    end
+  end
+end, { desc = 'Delete current file' })
+
 -- MARKDOWN KEYMAPS
 map('n', '<leader>sc', ':Telescope bibtex<CR>', { desc = 'Search BibTeX with Telescope' })
 
@@ -37,7 +61,7 @@ vim.keymap.set('n', '<leader>zt', '<Cmd>ZkTags<CR>', opts)
 -- Search for the notes matching a given query.
 -- vim.keymap.set('n', '<leader>zf', "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
 -- Search for the notes matching the current visual selection.
-vim.keymap.set('v', '<leader>zf', ":'<,'>ZkMatch<CR>", opts)
+vim.keymap.set('v', '<leader>zF', ":'<,'>ZkMatch<CR>", opts)
 
 vim.keymap.set('n', '<leader>zb', ':<Cmd>ZkBacklinks<CR>', opts)
 
