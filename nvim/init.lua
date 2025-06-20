dc496f273fdb89af4eb149fb0095326a78df49bb
Abraham Bahez <s.abrahambh@gmail.com>
2025-06-20 13:44:14 -0600

    Fix: (Nvim) Add prettierc options to formatter(conform) in config

--- nvim/init.lua
+++ nvim/init.lua
@@ -730,6 +730,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettierd', 'prettier' },
+        javascriptreact = { 'prettierd', 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
