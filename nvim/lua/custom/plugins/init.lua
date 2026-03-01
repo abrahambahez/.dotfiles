37bb588ba46e683e8554ce4e62df6a1f7135fbe2
Sergio Barrera <s.abrahambh@gmail.com>
2026-03-01 16:37:19 -0600

    Fix: Default obsidian.nvim plugin undesired behavior

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -73,12 +73,13 @@ return {
       },
       picker = { name = 'telescope.nvim' },
       preferred_link_style = 'wiki',
-      note_id_func = function(title)
-        return title or tostring(os.time())
-      end,
-      templates = { folder = 'templates' },
+      -- note_id_func = function(title)
+      --   return title or tostring(os.time())
+      -- end,
+      templates = { folder = nil },
       completion = { nvim_cmp = true, min_chars = 2 },
       legacy_commands = false,
+      frontmatter = { enabled = false },
     },
   },
 }
